38b2369e9d528137a37baf4f87c115043080a863
dkingalding <2502309334@qq.com>
2018-07-09 21:37:49 +0800

    controller

---
+++ admin/admin_extend/controller/index.php
@@ -0,0 +1,8 @@
+<?php 
+namespace app\admin_extend\controller;
+use think\Controller;
+class index extends Controller
+{
+
+     
+}
---
+++ admin/admin_extend/controller/wxget.php
@@ -0,0 +1,7 @@
+<?php
+  namespace app\admin_extend\controller;
+  use think\Controller;
+  class wxget extends Controller
+  {
+     
+  }
